f444ce41dd8a798de80ae30a93ccb1c8cc0f3b6e
YusifMorley <yusifmorley@gmail.com>
2024-09-24 18:22:46 +0800

    基本完毕

--- server.ts
+++ server.ts
@@ -32,9 +32,26 @@ const targetBl = 'public/tempelete/tohuemodle/desktop/black'; // 替换为你的
 const targetWh='public/tempelete/tohuemodle/desktop/white'
 checkDireDe(targetBl);
 checkDireDe(targetWh);
+
+
+
 const ip="167.179.118.142"
 const port=3000;
 let  app = express()
+app.all("*", (req, res, next) => {
+    // 设置允许跨域的域名，*代表允许任意域名跨域
+    res.header("Access-Control-Allow-Origin","*");
+    // 允许的header类型
+    res.header("Access-Control-Allow-Headers","content-type");
+    // 跨域允许的请求方式
+    res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
+    if (req.method.toLowerCase() == 'options')
+        res.send(200);  // 让options尝试请求快速结束
+    else
+        next();
+});
+
+
 const staticPath="public/tempelete/tohuemodle"
 app.use(cors())
 app.use(express.static(staticPath))
