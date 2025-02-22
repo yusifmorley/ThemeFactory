a0c4b79257a5e0607f6c9e01845634ab60e3ffa9
YusifMorley <yusifmorley@gmail.com>
2025-02-22 14:41:08 +0800

    日志功能

--- src/db/mysql-use.ts
+++ src/db/mysql-use.ts
@@ -2,7 +2,8 @@ import {Sequelize} from "sequelize";
 import {dbConfig,dbConfigPro,DBConfig} from "../../config";
 import process from "node:process";
 import {initModels, theme_editor_logAttributes} from "./models/init-models";
-
+import logger from "../lib/config/log_config";
+let log=logger.getLogger(`${__filename}`);
 let db:DBConfig = dbConfig;
 if(process.env.NODE_ENV!=="dev"){
     db=dbConfigPro
@@ -14,5 +15,7 @@ const sequelize = new Sequelize('theme_factory',
         host: 'localhost',
         dialect: 'mysql'/* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
 });
-
+sequelize.authenticate().then(()=>{
+    log.info("数据库连接成功")
+});
  export default initModels(sequelize) ;
